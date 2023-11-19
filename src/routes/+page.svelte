a7c9afc7d98e9fa8a227b5afa14b7ca55e2f74e7
Martin Jablečník <martin.jablecnik@email.cz>
2023-11-19 17:33:55 +0100

    Init project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+  let count = $state(0);
+  
+  function increment() {
+      count += 1;
+  }
+</script>
+
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<br>
+<h1 class="text-3xl font-bold underline">
+  Hello world!
+</h1>
+<br>
+
+<button on:click={increment} class='bg-blue-500 w-[200px] h-[50px]'>
+  clicks: {count}
+</button>
