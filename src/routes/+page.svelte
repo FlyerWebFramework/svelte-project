26d3943ba858a611e9a28ab41d7609dcf92f19db
Martin Jablečník <martin.jablecnik@email.cz>
2024-01-05 11:14:21 +0100

    Add typography

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,6 +15,9 @@
   Hello world!
 </h1>
 <br>
+<article class='prose'>
+<h2>Garlic bread with cheese: What the science tells us</h2>
+</article>
 
 <button on:click={increment} class='bg-blue-500 w-[200px] h-[50px]'>
   clicks: {count}
